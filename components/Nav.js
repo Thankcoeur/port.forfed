b18f9ac633f338b2e03076bbc7bb0549a91a6367
Thankcoeur <43066804+Thankcoeur@users.noreply.github.com>
2022-04-21 09:52:31 +0200

    mmmmm

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ export default  function Nav() {
 
 return (
     <nav id="navbar"  className="md:grid-cols-2 md:grid">
-    <div className="text-4xl font-bold">Forfed . <span className="text-orange-500">port</span></div>
+    <div className="text-4xl font-bold">Forfed . <span className="">port</span></div>
     <ul className="flex justify-around items-center">
        <li> <a className="font-semibold" href="tel:+33685037370">06 85 03 73 70</a></li>
        <li><a className="font-semibold" href="mailto:maxime.fedou@gmail.com" >maxime.fedou@gmail.com</a></li>
